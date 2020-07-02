a3c14c46c109c1a85fbd7632d033ac7ee35fa744
Matthew Lane <themplane@gmail.com>
2020-07-01 18:23:48 -0700

    finish meal.js, order.js, unskip all tests in meal-test.js, order-test.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,40 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(ingredient, arr) {
+  (arr.includes(ingredient)) ? '' : arr.push(ingredient)
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  return price * 0.9
+}
+
+function createRecipe(title, arr, type) {
+  return {
+    title: title,
+    ingredients: arr,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,28 @@
+function takeOrder(order, arr) {
+  (arr.length < 3) ? arr.push(order): ''
+}
+
+function refundOrder(pos, arr) {
+  for (var i = 0, run = arr.length; i < run; i++) {
+    (arr[i] == undefined || arr[i].orderNumber != pos) ? arr.push(arr.shift()): arr.shift()
+  }
+}
+
+function listItems(arr) {
+  return arr.reduce((sum, cur) => {
+    return `${sum}, ${cur.item}`
+  }, ``).slice(2)
+}
+
+function searchOrder(arr, item) {
+  return arr.map((cur) => {
+    return cur.item === item
+  }).includes(true)
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
