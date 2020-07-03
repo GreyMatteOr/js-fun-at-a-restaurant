edfa851305a717d39a8c9d39420b89c06ec2420f
Matthew Lane <themplane@gmail.com>
2020-07-03 07:50:59 -0700

    create a profits tracking property in restaurant1 and add necessary tests

--- src/restaurant1.js
+++ src/restaurant1.js
@@ -1,15 +1,50 @@
 var restaurant = (() => {
-  var globals = {}
+  var _globals = {
+    _cityTaxes : 0
+  }
 
   function construct(name) {
     this.name = name
+    this.foodCost = 0
+    this._revenue = 0
+    this._taxesDue = 0
+    Object.defineProperty(this, 'revenue', {
+      get(){
+        return floorToCents(this._revenue)
+      },
+      set(x){
+        this.taxesDue += calculateTaxes(x - this._revenue)
+        this._revenue = x
+      }
+    })
+    Object.defineProperty(this, 'taxesDue', {
+      get(){
+        return floorToCents(this._taxesDue)
+      },
+      set(x){
+        this.cityTaxes += x - this._taxesDue
+        this._taxesDue = x
+      }
+    })
+    Object.defineProperty(this, 'cityTaxes', {
+      get() {
+        return floorToCents(_globals._cityTaxes)
+      },
+      set(x) {
+        _globals._cityTaxes = x
+      }
+    })
+    Object.defineProperty(this, 'profit', {
+      get() {
+        return floorToCents(this.revenue - this.foodCost - this.taxesDue)
+      }
+    })
     this.menus = {
       breakfast: [],
       lunch: [],
       dinner: []
     }
     this.itemRefs = {}
-
   }
   return construct
 })()
@@ -24,20 +59,45 @@ function addMenuItem(rest, item) {
 }
 
 function removeMenuItem(rest, itemName) {
-  console.log(rest.itemRefs[itemName]);
   return (rest.itemRefs[itemName] == undefined) ? `Sorry, we don't sell ${itemName}, try adding a new recipe!` : _removeMenuItem(rest, itemName)
 }
 
 function _removeMenuItem(rest, itemName) {
   var arr = rest.menus[rest.itemRefs[itemName]]
   for (var i = 0, run = arr.length; i < run; i++) {
-    (arr[i].name == itemName) ? arr.shift() : arr.push(arr.shift())
+    (arr[i].name == itemName) ? arr.shift(): arr.push(arr.shift())
   }
   return `No one is eating our ${itemName} - it has been removed from the ${rest.itemRefs[itemName]} menu!`
+}
+
+function orderMenuItem(rest) {
+  const menuKeys = Object.keys(rest.menus)
+  var menu = Math.floor((menuKeys.length) * Math.random())
+  menu = menuKeys[menu]
+  var item = Math.floor((rest.menus[menu].length) * Math.random())
+  item = rest.menus[menu][item]
+  _recordMoney(rest, item)
+  return item
+}
+
+function _recordMoney(rest, item) {
+  rest.foodCost += 5
+  rest.revenue += floorToCents(item.price)
+}
+
+function floorToCents(price){
+   return(Math.floor(Number(price) * 100))/100
+}
+
+function calculateTaxes(price){
+  return (Number(price)/10)
 }
 
 module.exports = {
   createRestaurant,
   addMenuItem,
-  removeMenuItem
+  removeMenuItem,
+  orderMenuItem,
+  floorToCents,
+  calculateTaxes
 }
--- test/restaurant-test1.js
+++ test/restaurant-test1.js
@@ -5,6 +5,9 @@ var {
   createRestaurant,
   addMenuItem,
   removeMenuItem,
+  orderMenuItem,
+  floorToCents,
+  calculateTaxes
 } = require("../src/restaurant1");
 
 describe("restaurant.js", function() {
@@ -183,5 +186,194 @@ describe("restaurant.js", function() {
 
       assert.equal(error, "Sorry, we don't sell Mom's Spaghetti, try adding a new recipe!");
     });
+
   });
+  describe("orderMenuItem", function() {
+    it('should be a function', function() {
+      assert.isFunction(orderMenuItem)
+    })
+    it('should pick a Menu Item', function() {
+      var pizzaRestaurant = createRestaurant("Sexy Pizza");
+      var bbqPizza = {
+        name: "BBQ Chicken Pizza",
+        price: "12.49",
+        type: "lunch"
+      };
+
+      var veggiePizza = {
+        name: "Veggie Pizza",
+        price: "11.49",
+        type: "dinner"
+      };
+
+      var baconEggsPizza = {
+        name: "Bacon and Eggs Pizza",
+        price: "13.49",
+        type: "breakfast"
+      };
+
+      addMenuItem(pizzaRestaurant, bbqPizza);
+      addMenuItem(pizzaRestaurant, baconEggsPizza);
+      addMenuItem(pizzaRestaurant, veggiePizza);
+
+      const order = orderMenuItem(pizzaRestaurant)
+
+      const inBreakfast = pizzaRestaurant.menus.breakfast.includes(order)
+      const inLunch = pizzaRestaurant.menus.lunch.includes(order)
+      const inDinner = pizzaRestaurant.menus.dinner.includes(order)
+
+      assert.isTrue(inBreakfast || inLunch || inDinner)
+    })
+    it('it should record in the restaurant taxesDue, revenue, and foodCost', function() {
+      var pizzaRestaurant = createRestaurant("Sexy Pizza");
+      var bbqPizza = {
+        name: "BBQ Chicken Pizza",
+        price: "12.49",
+        type: "lunch"
+      };
+
+      var veggiePizza = {
+        name: "Veggie Pizza",
+        price: "11.49",
+        type: "dinner"
+      };
+
+      var baconEggsPizza = {
+        name: "Bacon and Eggs Pizza",
+        price: "13.49",
+        type: "breakfast"
+      };
+
+      addMenuItem(pizzaRestaurant, bbqPizza);
+      addMenuItem(pizzaRestaurant, baconEggsPizza);
+      addMenuItem(pizzaRestaurant, veggiePizza);
+
+      const order = orderMenuItem(pizzaRestaurant)
+      assert.equal(pizzaRestaurant.foodCost, 5)
+      assert.equal(pizzaRestaurant.revenue, floorToCents(order.price))
+      assert.closeTo(pizzaRestaurant.taxesDue, calculateTaxes(order.price), .01)
+    })
+    it('it should record these things for many orders', function() {
+      var pizzaRestaurant = createRestaurant("Sexy Pizza");
+      var bbqPizza = {
+        name: "BBQ Chicken Pizza",
+        price: "12.49",
+        type: "lunch"
+      };
+
+      var veggiePizza = {
+        name: "Veggie Pizza",
+        price: "11.49",
+        type: "dinner"
+      };
+
+      var baconEggsPizza = {
+        name: "Bacon and Eggs Pizza",
+        price: "13.49",
+        type: "breakfast"
+      };
+
+      addMenuItem(pizzaRestaurant, bbqPizza);
+      addMenuItem(pizzaRestaurant, baconEggsPizza);
+      addMenuItem(pizzaRestaurant, veggiePizza);
+
+      const order1 = orderMenuItem(pizzaRestaurant)
+      const order2 = orderMenuItem(pizzaRestaurant)
+      assert.equal(pizzaRestaurant.foodCost, 10)
+      assert.equal(pizzaRestaurant.revenue, floorToCents(order1.price) + floorToCents(order2.price))
+      assert.closeTo(pizzaRestaurant.taxesDue, (floorToCents(calculateTaxes(order1.price) + calculateTaxes(order2.price))), .011)
+    })
+    it('should be possible to find the restaurants total profit', function() {
+      var pizzaRestaurant = createRestaurant("Sexy Pizza");
+      var bbqPizza = {
+        name: "BBQ Chicken Pizza",
+        price: "12.49",
+        type: "lunch"
+      };
+
+      var veggiePizza = {
+        name: "Veggie Pizza",
+        price: "11.49",
+        type: "dinner"
+      };
+
+      var baconEggsPizza = {
+        name: "Bacon and Eggs Pizza",
+        price: "13.49",
+        type: "breakfast"
+      };
+
+      addMenuItem(pizzaRestaurant, bbqPizza);
+      addMenuItem(pizzaRestaurant, baconEggsPizza);
+      addMenuItem(pizzaRestaurant, veggiePizza);
+
+      const order1 = orderMenuItem(pizzaRestaurant)
+      const financials1 = order1.price - 5 - calculateTaxes(order1.price)
+      assert.closeTo(pizzaRestaurant.profit, floorToCents(financials1), .02)
+
+      const order2 = orderMenuItem(pizzaRestaurant)
+      const financials2 = order2.price - 5 - calculateTaxes(order2.price)
+      assert.closeTo(pizzaRestaurant.profit, floorToCents(financials1 + financials2), .021)
+    })
+    it('should track the total taxesDue to the government', function() {
+      var pizzaRestaurant = createRestaurant("Sexy Pizza");
+      var bbqPizza = {
+        name: "BBQ Chicken Pizza",
+        price: "12.49",
+        type: "lunch"
+      };
+
+      var veggiePizza = {
+        name: "Veggie Pizza",
+        price: "11.49",
+        type: "dinner"
+      };
+
+      var baconEggsPizza = {
+        name: "Bacon and Eggs Pizza",
+        price: "13.49",
+        type: "breakfast"
+      };
+
+      addMenuItem(pizzaRestaurant, bbqPizza);
+      addMenuItem(pizzaRestaurant, baconEggsPizza);
+      addMenuItem(pizzaRestaurant, veggiePizza);
+
+      var tacoRestaurant = createRestaurant("Sexy Taco");
+      var bbqTaco = {
+        name: "BBQ Chicken Taco",
+        price: "12.49",
+        type: "lunch"
+      };
+
+      var veggieTaco = {
+        name: "Veggie Taco",
+        price: "11.49",
+        type: "dinner"
+      };
+
+      var baconEggsTaco = {
+        name: "Bacon and Eggs Taco",
+        price: "13.49",
+        type: "breakfast"
+      };
+
+      addMenuItem(tacoRestaurant, bbqTaco);
+      addMenuItem(tacoRestaurant, baconEggsTaco);
+      addMenuItem(tacoRestaurant, veggieTaco);
+      pizzaRestaurant.cityTaxes = 0
+      const order1 = orderMenuItem(tacoRestaurant)
+      const order2 = orderMenuItem(tacoRestaurant)
+      const order3 = orderMenuItem(pizzaRestaurant)
+      const order4 = orderMenuItem(pizzaRestaurant)
+
+      assert.equal(pizzaRestaurant.cityTaxes, tacoRestaurant.cityTaxes)
+
+      assert.closeTo(pizzaRestaurant.cityTaxes, floorToCents(tacoRestaurant.taxesDue + pizzaRestaurant.taxesDue), .021)
+
+      const cumulativeOrdersTax = calculateTaxes(order1.price) + calculateTaxes(order2.price) + calculateTaxes(order3.price) + calculateTaxes(order4.price)
+      assert.closeTo(pizzaRestaurant.cityTaxes, floorToCents(cumulativeOrdersTax), .051)
+
+    })
+  })
 });
