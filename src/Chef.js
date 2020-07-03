f65c80cda4571df821576c93e0c555141aef5074
Matthew Lane <themplane@gmail.com>
2020-07-03 09:04:44 -0700

    Create Chef.js, unskip and pass all Chef-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,17 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, rest) {
+    this.name = name
+    this.restaurant = rest
+  }
+
+  greetCustomer(name, isMorning = false){
+    return (isMorning) ? `Good morning, ${name}!` : `Hello, ${name}!`
+  }
+
+  checkForFood(item){
+    return (Object.keys(this.restaurant.menus).reduce((isInPrev, cur) => {
+      return this.restaurant.menus[cur].includes(item) || isInPrev
+    }, false)) ? `Yes, we're serving ${item.name} today!` : `Sorry, we aren't serving ${item.name} today.`
   }
 }
 
