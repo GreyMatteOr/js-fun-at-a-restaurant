88a1000ffa352526f0cac8081e5d9c6dafe28bcd
Matthew Lane <themplane@gmail.com>
2020-07-06 10:50:00 -0700

    Make order.js more readable

--- src/order.js
+++ src/order.js
@@ -9,14 +9,12 @@ function refundOrder(pos, arr) {
 }
 
 function listItems(arr) {
-  return arr.map((item) => item.item )
-            .reduce((sum, cur) => `${sum}, ${cur}` )
+  return arr.map((item) => item.item)
+            .reduce((sum, cur) => `${sum}, ${cur}`)
 }
 
 function searchOrder(arr, item) {
-  return arr.reduce((isInPrev ,cur) => {
-    return (cur.item === item) || isInPrev
-  }, false)
+  return arr.reduce((isInPrev, cur) => (cur.item === item) || isInPrev, false)
 }
 
 module.exports = {
